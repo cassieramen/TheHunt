093d25b592157bd10bd42621899fbde14701f2ca
NU CS Intern <nucsintern@shmoop-cs-intern-2.local>
2014-03-01 14:49:39 -0600

    Alex added framework for new feed

--- TheHunt/The Hunt/src/main/java/com/thehunt/app/HomeFeed.java
+++ TheHunt/The Hunt/src/main/java/com/thehunt/app/HomeFeed.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class HomeFeed extends Activity {
 
@@ -11,27 +12,9 @@ public class HomeFeed extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_feed);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_feed, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        ListView listView = (ListView) findViewById(R.id.home_feed_list);
+        HomeFeedAdapter homeFeedAdapter = new HomeFeedAdapter(getBaseContext());
+        listView.setAdapter(homeFeedAdapter);
     }
 
 }
---
+++ TheHunt/The Hunt/src/main/java/com/thehunt/app/HomeFeedAdapter.java
@@ -0,0 +1,43 @@
+package com.thehunt.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by nucsintern on 3/1/14.
+ */
+public class HomeFeedAdapter extends BaseAdapter {
+
+    private Context context;
+
+    public HomeFeedAdapter (Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.one_pic_two_text, null);
+        }
+        return view;
+    }
+}
