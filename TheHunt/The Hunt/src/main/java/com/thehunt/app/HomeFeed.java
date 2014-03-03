3fa864cfa5453adbd9411acfa65639333cc77a5a
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-03-02 20:21:42 -0600

    Shikhar added empty sliding menu

--- TheHunt/The Hunt/src/main/java/com/thehunt/app/HomeFeed.java
+++ TheHunt/The Hunt/src/main/java/com/thehunt/app/HomeFeed.java
@@ -1,22 +1,41 @@
 package com.thehunt.app;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+
 public class HomeFeed extends Activity {
 
+    private SlidingMenu slidingMenu;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_feed);
         ListView listView = (ListView) findViewById(R.id.home_feed_list);
         HomeFeedAdapter homeFeedAdapter = new HomeFeedAdapter(getBaseContext());
         listView.setAdapter(homeFeedAdapter);
+
+        slidingMenu = new SlidingMenu(this);
+
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setShadowWidthRes(R.dimen.slidingmenu_shadow_width);
+        slidingMenu.setShadowDrawable(R.drawable.shadow);
+        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        slidingMenu.setFadeDegree(0.35f);
+        slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        slidingMenu.setMenu(R.layout.slidingmenu);
+
+
     }
-
-
 
 }
