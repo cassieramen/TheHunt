bc58a03666ce9337483496f5384d06c5f150a0c4
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-03-16 09:13:11 -0500

    added sign-in page

---
+++ TheHunt/The Hunt/src/main/java/com/thehunt/app/SignIn.java
@@ -0,0 +1,37 @@
+package com.thehunt.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by home on 3/16/14.
+ */
+public class SignIn extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+
+        TextView txt = (TextView) findViewById(R.id.headerText);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
+        txt.setTypeface(font);
+
+
+        Button login = (Button) findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), HomeFeed.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
