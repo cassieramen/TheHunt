c8c0fef8420effb4b89041049cab11a080eea711
NU CS Intern <nucsintern@shmoop-cs-intern-2.local>
2014-03-16 17:28:30 -0500

    Fixed create feed item layout, added fields and listeners to create home screen

--- TheHunt/The Hunt/src/main/java/com/thehunt/app/CreateAHunt.java
+++ TheHunt/The Hunt/src/main/java/com/thehunt/app/CreateAHunt.java
@@ -18,8 +18,42 @@ public class CreateAHunt extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_home);
 
-        final Slider menu = new Slider(this);
 
+
+
+        final Slider menu = new Slider(this);
+        TextView inviteFriends = (TextView) findViewById(R.id.invite);
+        MenuItemClickListener inviteListener  = new MenuItemClickListener(getBaseContext(), inviteFriends);
+        inviteFriends.setOnClickListener(inviteListener);
+        TextView addLocation = (TextView) findViewById(R.id.location);
+        MenuItemClickListener locationListener  = new MenuItemClickListener(getBaseContext(), addLocation);
+        addLocation.setOnClickListener(locationListener);
+        
+
+
+    }
+
+    class MenuItemClickListener implements View.OnClickListener {
+        Context context;
+        TextView textView;
+        MenuItemClickListener(Context context, TextView textView) {
+            this.context = context;
+            this.textView = textView;
+        }
+
+        public void onClick(View view) {
+            if (this.textView.getId() == R.id.invite) {
+                Intent intent = new Intent(context, HuntDescription.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+            else if (this.textView.getId() == R.id.tasks) {
+                Intent intent = new Intent(context, HuntDescription.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+
+        }
     }
 
 
