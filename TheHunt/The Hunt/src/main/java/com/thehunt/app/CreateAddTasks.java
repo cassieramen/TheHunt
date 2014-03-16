9271e116c7c47a13ad465c8913949a211bf5b78c
Alex Hollenbeck <nucsintern@shmoop-cs-intern-2.local>
2014-03-16 17:51:03 -0500

    Added framework for inviting friends and adding tasks

---
+++ TheHunt/The Hunt/src/main/java/com/thehunt/app/CreateAddTasks.java
@@ -0,0 +1,27 @@
+package com.thehunt.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by nucsintern on 3/16/14.
+ */
+public class CreateAddTasks extends Activity {
+
+    @ Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_home);
+
+
+
+
+        final Slider menu = new Slider(this);
+
+
+
+    }
+
+
+}
---
+++ TheHunt/The Hunt/src/main/java/com/thehunt/app/CreateInviteFriends.java
@@ -0,0 +1,26 @@
+package com.thehunt.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nucsintern on 3/16/14.
+ */
+public class CreateInviteFriends extends Activity {
+
+    @ Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_home);
+
+
+
+
+        final Slider menu = new Slider(this);
+
+
+
+    }
+
+
+}
